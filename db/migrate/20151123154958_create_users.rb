7916261bd8e41d61d149f8efd7cbd63ab319893a
cjmanetta <cjmanetta@gmail.com>
2015-11-23 12:00:35 -0500

    generate user model, add user migrations

---
+++ db/migrate/20151123154958_create_users.rb
@@ -0,0 +1,20 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :age
+      t.boolean :basic_start
+      t.integer :days_of_7_vig
+      t.integer :mins_vig_per_day
+      t.integer :days_of_7_mod
+      t.integer :mins_mod_per_day
+      t.integer :days_of_7_walk
+      t.integer :mins_walk_per_day
+      t.integer :mins_weekday_sitting
+
+      t.timestamps null: false
+    end
+  end
+end
