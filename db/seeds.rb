0231d520dc2c25b21e170c12d49c2b54ea857a3a
cjmanetta <cjmanetta@gmail.com>
2015-11-23 16:54:20 -0500

    stub out user IPAQ survey results

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,4 +28,150 @@ end
 
 User.third.basic_start = true
 
+User.find(1).days_of_7_vig = 0
+User.find(1).mins_vig_per_day = 0
+User.find(1).days_of_7_mod = 0
+User.find(1).mins_mod_per_day = 0
+User.find(1).days_of_7_walk = 0
+User.find(1).mins_walk_per_day = 0
+User.find(1).mins_weekday_sitting = 0
+
+User.find(2).days_of_7_vig = 0
+User.find(2).mins_vig_per_day = 0
+User.find(2).days_of_7_mod = 0
+User.find(2).mins_mod_per_day = 0
+User.find(2).days_of_7_walk = 0
+User.find(2).mins_walk_per_day = 0
+User.find(2).mins_weekday_sitting = 0
+
+User.find(3).days_of_7_vig = 0
+User.find(3).mins_vig_per_day = 0
+User.find(3).days_of_7_mod = 0
+User.find(3).mins_mod_per_day = 0
+User.find(3).days_of_7_walk = 0
+User.find(3).mins_walk_per_day = 0
+User.find(3).mins_weekday_sitting = 0
+
+User.find(4).days_of_7_vig = 0
+User.find(4).mins_vig_per_day = 0
+User.find(4).days_of_7_mod = 0
+User.find(4).mins_mod_per_day = 0
+User.find(4).days_of_7_walk = 0
+User.find(4).mins_walk_per_day = 0
+User.find(4).mins_weekday_sitting = 0
+
+User.find(5).days_of_7_vig = 0
+User.find(5).mins_vig_per_day = 0
+User.find(5).days_of_7_mod = 0
+User.find(5).mins_mod_per_day = 0
+User.find(5).days_of_7_walk = 0
+User.find(5).mins_walk_per_day = 0
+User.find(5).mins_weekday_sitting = 0
+
+User.find(6).days_of_7_vig = 0
+User.find(6).mins_vig_per_day = 0
+User.find(6).days_of_7_mod = 0
+User.find(6).mins_mod_per_day = 0
+User.find(6).days_of_7_walk = 0
+User.find(6).mins_walk_per_day = 0
+User.find(6).mins_weekday_sitting = 0
+
+User.find(7).days_of_7_vig = 0
+User.find(7).mins_vig_per_day = 0
+User.find(7).days_of_7_mod = 0
+User.find(7).mins_mod_per_day = 0
+User.find(7).days_of_7_walk = 0
+User.find(7).mins_walk_per_day = 0
+User.find(7).mins_weekday_sitting = 0
+
+User.find(8).days_of_7_vig = 0
+User.find(8).mins_vig_per_day = 0
+User.find(8).days_of_7_mod = 0
+User.find(8).mins_mod_per_day = 0
+User.find(8).days_of_7_walk = 0
+User.find(8).mins_walk_per_day = 0
+User.find(8).mins_weekday_sitting = 0
+
+User.find(9).days_of_7_vig = 0
+User.find(9).mins_vig_per_day = 0
+User.find(9).days_of_7_mod = 0
+User.find(9).mins_mod_per_day = 0
+User.find(9).days_of_7_walk = 0
+User.find(9).mins_walk_per_day = 0
+User.find(9).mins_weekday_sitting = 0
+
+User.find(10).days_of_7_vig = 0
+User.find(10).mins_vig_per_day = 0
+User.find(10).days_of_7_mod = 0
+User.find(10).mins_mod_per_day = 0
+User.find(10).days_of_7_walk = 0
+User.find(10).mins_walk_per_day = 0
+User.find(10).mins_weekday_sitting = 0
+
+User.find(11).days_of_7_vig = 0
+User.find(11).mins_vig_per_day = 0
+User.find(11).days_of_7_mod = 0
+User.find(11).mins_mod_per_day = 0
+User.find(11).days_of_7_walk = 0
+User.find(11).mins_walk_per_day = 0
+User.find(11).mins_weekday_sitting = 0
+
+User.find(12).days_of_7_vig = 0
+User.find(12).mins_vig_per_day = 0
+User.find(12).days_of_7_mod = 0
+User.find(12).mins_mod_per_day = 0
+User.find(12).days_of_7_walk = 0
+User.find(12).mins_walk_per_day = 0
+User.find(12).mins_weekday_sitting = 0
+
+User.find(13).days_of_7_vig = 0
+User.find(13).mins_vig_per_day = 0
+User.find(13).days_of_7_mod = 0
+User.find(13).mins_mod_per_day = 0
+User.find(13).days_of_7_walk = 0
+User.find(13).mins_walk_per_day = 0
+User.find(13).mins_weekday_sitting = 0
+
+User.find(14).days_of_7_vig = 0
+User.find(14).mins_vig_per_day = 0
+User.find(14).days_of_7_mod = 0
+User.find(14).mins_mod_per_day = 0
+User.find(14).days_of_7_walk = 0
+User.find(14).mins_walk_per_day = 0
+User.find(14).mins_weekday_sitting = 0
+
+User.find(15).days_of_7_vig = 0
+User.find(15).mins_vig_per_day = 0
+User.find(15).days_of_7_mod = 0
+User.find(15).mins_mod_per_day = 0
+User.find(15).days_of_7_walk = 0
+User.find(15).mins_walk_per_day = 0
+User.find(15).mins_weekday_sitting = 0
+
+User.find(16).days_of_7_vig = 0
+User.find(16).mins_vig_per_day = 0
+User.find(16).days_of_7_mod = 0
+User.find(16).mins_mod_per_day = 0
+User.find(16).days_of_7_walk = 0
+User.find(16).mins_walk_per_day = 0
+User.find(16).mins_weekday_sitting = 0
+
+User.find(17).days_of_7_vig = 0
+User.find(17).mins_vig_per_day = 0
+User.find(17).days_of_7_mod = 0
+User.find(17).mins_mod_per_day = 0
+User.find(17).days_of_7_walk = 0
+User.find(17).mins_walk_per_day = 0
+User.find(17).mins_weekday_sitting = 0
+
+User.find(18).days_of_7_vig = 0
+User.find(18).mins_vig_per_day = 0
+User.find(18).days_of_7_mod = 0
+User.find(18).mins_mod_per_day = 0
+User.find(18).days_of_7_walk = 0
+User.find(18).mins_walk_per_day = 0
+User.find(18).mins_weekday_sitting = 0
+
+
+
 
