476aa6e317ebc878d6be13727bfa679cb681890f
Peter Manetta <pbmanetta@gmail.com>
2015-11-25 13:05:37 -0700

    stub data for ipaq survey: basically I added ipaq data for 18 fake users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,3 @@
-User.create(
-  first_name: "Peter",
-  last_name: "Manetta",
-  email: "pmanetta@gmail.com",
-  admin: true,
-  password: "1234",
-  age: 35,
-  basic_start: false)
-
-User.create(
-  first_name: "Charlotte",
-  last_name: "Manetta",
-  email: "cjmanetta@gmail.com",
-  admin: true,
-  password: "1234",
-  age: 31,
-  basic_start: false)
-
 18.times do
   User.create(
     first_name: Faker::Name.first_name,
@@ -38,51 +20,51 @@ User.find(1).mins_weekday_sitting = 0
 
 User.find(2).days_of_7_vig = 0
 User.find(2).mins_vig_per_day = 0
-User.find(2).days_of_7_mod = 0
-User.find(2).mins_mod_per_day = 0
-User.find(2).days_of_7_walk = 0
-User.find(2).mins_walk_per_day = 0
-User.find(2).mins_weekday_sitting = 0
+User.find(2).days_of_7_mod = 3
+User.find(2).mins_mod_per_day = 30
+User.find(2).days_of_7_walk = 4
+User.find(2).mins_walk_per_day = 40
+User.find(2).mins_weekday_sitting = 400
 
-User.find(3).days_of_7_vig = 0
-User.find(3).mins_vig_per_day = 0
-User.find(3).days_of_7_mod = 0
-User.find(3).mins_mod_per_day = 0
-User.find(3).days_of_7_walk = 0
-User.find(3).mins_walk_per_day = 0
-User.find(3).mins_weekday_sitting = 0
+User.find(3).days_of_7_vig = 2
+User.find(3).mins_vig_per_day = 40
+User.find(3).days_of_7_mod = 3
+User.find(3).mins_mod_per_day = 60
+User.find(3).days_of_7_walk = 2
+User.find(3).mins_walk_per_day = 20
+User.find(3).mins_weekday_sitting = 350
 
 User.find(4).days_of_7_vig = 0
 User.find(4).mins_vig_per_day = 0
 User.find(4).days_of_7_mod = 0
 User.find(4).mins_mod_per_day = 0
-User.find(4).days_of_7_walk = 0
-User.find(4).mins_walk_per_day = 0
-User.find(4).mins_weekday_sitting = 0
+User.find(4).days_of_7_walk = 3
+User.find(4).mins_walk_per_day = 20
+User.find(4).mins_weekday_sitting = 600
 
-User.find(5).days_of_7_vig = 0
-User.find(5).mins_vig_per_day = 0
-User.find(5).days_of_7_mod = 0
-User.find(5).mins_mod_per_day = 0
-User.find(5).days_of_7_walk = 0
-User.find(5).mins_walk_per_day = 0
-User.find(5).mins_weekday_sitting = 0
+User.find(5).days_of_7_vig = 1
+User.find(5).mins_vig_per_day = 30
+User.find(5).days_of_7_mod = 4
+User.find(5).mins_mod_per_day = 60
+User.find(5).days_of_7_walk = 3
+User.find(5).mins_walk_per_day = 40
+User.find(5).mins_weekday_sitting = 500
 
 User.find(6).days_of_7_vig = 0
 User.find(6).mins_vig_per_day = 0
-User.find(6).days_of_7_mod = 0
-User.find(6).mins_mod_per_day = 0
-User.find(6).days_of_7_walk = 0
-User.find(6).mins_walk_per_day = 0
-User.find(6).mins_weekday_sitting = 0
+User.find(6).days_of_7_mod = 2
+User.find(6).mins_mod_per_day = 30
+User.find(6).days_of_7_walk = 5
+User.find(6).mins_walk_per_day = 60
+User.find(6).mins_weekday_sitting = 700
 
 User.find(7).days_of_7_vig = 0
 User.find(7).mins_vig_per_day = 0
-User.find(7).days_of_7_mod = 0
-User.find(7).mins_mod_per_day = 0
-User.find(7).days_of_7_walk = 0
-User.find(7).mins_walk_per_day = 0
-User.find(7).mins_weekday_sitting = 0
+User.find(7).days_of_7_mod = 2
+User.find(7).mins_mod_per_day = 30
+User.find(7).days_of_7_walk = 2
+User.find(7).mins_walk_per_day = 20
+User.find(7).mins_weekday_sitting = 550
 
 User.find(8).days_of_7_vig = 0
 User.find(8).mins_vig_per_day = 0
@@ -90,88 +72,104 @@ User.find(8).days_of_7_mod = 0
 User.find(8).mins_mod_per_day = 0
 User.find(8).days_of_7_walk = 0
 User.find(8).mins_walk_per_day = 0
-User.find(8).mins_weekday_sitting = 0
+User.find(8).mins_weekday_sitting = 800
 
 User.find(9).days_of_7_vig = 0
 User.find(9).mins_vig_per_day = 0
-User.find(9).days_of_7_mod = 0
-User.find(9).mins_mod_per_day = 0
-User.find(9).days_of_7_walk = 0
-User.find(9).mins_walk_per_day = 0
-User.find(9).mins_weekday_sitting = 0
+User.find(9).days_of_7_mod = 2
+User.find(9).mins_mod_per_day = 40
+User.find(9).days_of_7_walk = 4
+User.find(9).mins_walk_per_day = 30
+User.find(9).mins_weekday_sitting = 650
 
-User.find(10).days_of_7_vig = 0
-User.find(10).mins_vig_per_day = 0
-User.find(10).days_of_7_mod = 0
-User.find(10).mins_mod_per_day = 0
-User.find(10).days_of_7_walk = 0
-User.find(10).mins_walk_per_day = 0
-User.find(10).mins_weekday_sitting = 0
+User.find(10).days_of_7_vig = 2
+User.find(10).mins_vig_per_day = 40
+User.find(10).days_of_7_mod = 2
+User.find(10).mins_mod_per_day = 60
+User.find(10).days_of_7_walk = 3
+User.find(10).mins_walk_per_day = 30
+User.find(10).mins_weekday_sitting = 400
 
-User.find(11).days_of_7_vig = 0
-User.find(11).mins_vig_per_day = 0
-User.find(11).days_of_7_mod = 0
-User.find(11).mins_mod_per_day = 0
-User.find(11).days_of_7_walk = 0
-User.find(11).mins_walk_per_day = 0
-User.find(11).mins_weekday_sitting = 0
+User.find(11).days_of_7_vig = 1
+User.find(11).mins_vig_per_day = 40
+User.find(11).days_of_7_mod = 5
+User.find(11).mins_mod_per_day = 60
+User.find(11).days_of_7_walk = 1
+User.find(11).mins_walk_per_day = 120
+User.find(11).mins_weekday_sitting = 880
 
 User.find(12).days_of_7_vig = 0
 User.find(12).mins_vig_per_day = 0
-User.find(12).days_of_7_mod = 0
-User.find(12).mins_mod_per_day = 0
-User.find(12).days_of_7_walk = 0
-User.find(12).mins_walk_per_day = 0
-User.find(12).mins_weekday_sitting = 0
+User.find(12).days_of_7_mod = 2
+User.find(12).mins_mod_per_day = 60
+User.find(12).days_of_7_walk = 2
+User.find(12).mins_walk_per_day = 50
+User.find(12).mins_weekday_sitting = 700
 
 User.find(13).days_of_7_vig = 0
 User.find(13).mins_vig_per_day = 0
 User.find(13).days_of_7_mod = 0
 User.find(13).mins_mod_per_day = 0
-User.find(13).days_of_7_walk = 0
-User.find(13).mins_walk_per_day = 0
-User.find(13).mins_weekday_sitting = 0
+User.find(13).days_of_7_walk = 1
+User.find(13).mins_walk_per_day = 30
+User.find(13).mins_weekday_sitting = 1100
 
 User.find(14).days_of_7_vig = 0
 User.find(14).mins_vig_per_day = 0
-User.find(14).days_of_7_mod = 0
-User.find(14).mins_mod_per_day = 0
-User.find(14).days_of_7_walk = 0
-User.find(14).mins_walk_per_day = 0
-User.find(14).mins_weekday_sitting = 0
+User.find(14).days_of_7_mod = 4
+User.find(14).mins_mod_per_day = 40
+User.find(14).days_of_7_walk = 4
+User.find(14).mins_walk_per_day = 60
+User.find(14).mins_weekday_sitting = 550
 
 User.find(15).days_of_7_vig = 0
 User.find(15).mins_vig_per_day = 0
-User.find(15).days_of_7_mod = 0
-User.find(15).mins_mod_per_day = 0
-User.find(15).days_of_7_walk = 0
-User.find(15).mins_walk_per_day = 0
-User.find(15).mins_weekday_sitting = 0
+User.find(15).days_of_7_mod = 2
+User.find(15).mins_mod_per_day = 60
+User.find(15).days_of_7_walk = 5
+User.find(15).mins_walk_per_day = 40
+User.find(15).mins_weekday_sitting = 850
 
 User.find(16).days_of_7_vig = 0
 User.find(16).mins_vig_per_day = 0
-User.find(16).days_of_7_mod = 0
-User.find(16).mins_mod_per_day = 0
-User.find(16).days_of_7_walk = 0
-User.find(16).mins_walk_per_day = 0
-User.find(16).mins_weekday_sitting = 0
+User.find(16).days_of_7_mod = 3
+User.find(16).mins_mod_per_day = 30
+User.find(16).days_of_7_walk = 3
+User.find(16).mins_walk_per_day = 60
+User.find(16).mins_weekday_sitting = 750
 
-User.find(17).days_of_7_vig = 0
-User.find(17).mins_vig_per_day = 0
-User.find(17).days_of_7_mod = 0
-User.find(17).mins_mod_per_day = 0
-User.find(17).days_of_7_walk = 0
-User.find(17).mins_walk_per_day = 0
-User.find(17).mins_weekday_sitting = 0
+User.find(17).days_of_7_vig = 3
+User.find(17).mins_vig_per_day = 40
+User.find(17).days_of_7_mod = 4
+User.find(17).mins_mod_per_day = 60
+User.find(17).days_of_7_walk = 1
+User.find(17).mins_walk_per_day = 20
+User.find(17).mins_weekday_sitting = 950
 
 User.find(18).days_of_7_vig = 0
 User.find(18).mins_vig_per_day = 0
 User.find(18).days_of_7_mod = 0
 User.find(18).mins_mod_per_day = 0
-User.find(18).days_of_7_walk = 0
-User.find(18).mins_walk_per_day = 0
-User.find(18).mins_weekday_sitting = 0
+User.find(18).days_of_7_walk = 2
+User.find(18).mins_walk_per_day = 60
+User.find(18).mins_weekday_sitting = 1000
 
+User.create(
+  first_name: "Peter",
+  last_name: "Manetta",
+  email: "pmanetta@gmail.com",
+  admin: true,
+  password: "1234",
+  age: 35,
+  basic_start: false)
 
+User.create(
+  first_name: "Charlotte",
+  last_name: "Manetta",
+  email: "cjmanetta@gmail.com",
+  admin: true,
+  password: "1234",
+  age: 31,
+  basic_start: false)
 
 
