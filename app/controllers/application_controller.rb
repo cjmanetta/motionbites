7d70804e6b3b269837b0257708e9ffb0bd73c765
cjmanetta <cjmanetta@gmail.com>
2016-03-07 15:08:21 -0800

    add current_user and login methods to app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login(user)
+    session[:user_id] = user.id
+  end
+
+  def current_user
+    return nil if session[:user_id].blank?
+    @current_user ||= User.where(id: session[:user_id]).first
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,15 @@
 class SessionsController < ApplicationController
+  def new
+  end
+
   def create
     @user = User.find_by(email: params[:session][:email])
-    redirect_to user_path(@user.id)
+    if @user && @user.authenticate(params[:session][:password])
+      login(@user)
+      redirect_to user_path(@user.id)
+    else
+      flash[:notice] = "your username or password is incorrect"
+      redirect_to root_path
+    end
   end
 end
