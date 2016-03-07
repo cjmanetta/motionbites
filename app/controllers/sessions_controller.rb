530f5316085db1168f24f0a625dbe4417beeaa97
cjmanetta <cjmanetta@gmail.com>
2016-03-07 15:33:56 -0800

    add session_params method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
+    @user = User.find_by(email: session_params[:email])
+    if @user && @user.authenticate(session_params[:password])
       login(@user)
       redirect_to user_path(@user.id)
     else
       flash[:notice] = "your username or password is incorrect"
       redirect_to root_path
     end
+  end
+
+  private
+
+  def session_params
+    params.require(:session).permit(:email, :password)
   end
 end
