2fac4a3a61194b9909749dfe5f97b86490fb9172
cjmanetta <cjmanetta@gmail.com>
2016-03-27 16:36:29 -0700

    add incrementer to make scores unique for random selection

--- lib/exercise_score_calculator.rb
+++ lib/exercise_score_calculator.rb
@@ -1,11 +1,13 @@
 module ExerciseScoreCalculator
   def self.calculate_score(user, exercise)
     score = 100
-
+    incrementer = 0.001
     ADJUSTMENT_TABLE.each_key do |exercise_attribute|
       adjustment = ADJUSTMENT_TABLE.fetch(exercise_attribute)[user.fitness_score - 1][exercise.send(exercise_attribute) - 1]
       score += adjustment
     end
+    score += incrementer
+    incrementer += 0.001
 
     score
   end
--- spec/modules/exercise_score_calculator_spec.rb
+++ spec/modules/exercise_score_calculator_spec.rb
@@ -4,13 +4,13 @@ describe ExerciseScoreCalculator do
   let(:exercise) {FactoryGirl.build(:exercise)}
   let(:user) {FactoryGirl.build(:user)}
 
-  context "#calculate_score" do
+  context "#calculate_score", :focus do
 
     it "calculates the exercise score based on user and exercise data" do
 
       user.calculate_fitness_score
 
-      expect(ExerciseScoreCalculator.calculate_score(user, exercise)).to eq(160)
+      expect(ExerciseScoreCalculator.calculate_score(user, exercise)).to eq(160.001)
     end
   end
 
